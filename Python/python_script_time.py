0190aeec2e83d9652b161284d9dff94d83f23a5c
Martin <angers.martins@gmail.com>
2020-08-13 17:13:07 +0300

    20200813_17_15

--- Python/python_script_time.py
+++ Python/python_script_time.py
@@ -1,7 +1,46 @@
 #! /usr/bin/python3.8
+print("Ievadiet skaitli: ")
+# a=2**2000000
 
-a = int(input("Ievadiet skaitli: "))
-a = 2**a
+#a=int( input() )
 
-print(a)
+#if (type(a) == int): 
+#	print("a**10000000")
+#	print("complete")
+
+#if (a == 5): 
+#	print(a**100)
+
+#if isinstance(a, int): #works
+#	print(a**100)
+#	print("complete")
+
+paziime = False
+#Šie trīs ir derīgi varianti
+#while paziime==False: #(while paziime is False)
+#while paziime!= True: #(while paziime not True)
+while not paziime:
+    try:
+        a = int( input() )
+        paziime = True
+    except:
+        print("Diemžēl, cienījamais lietotāj, to, kas ievdīts, nevar",\
+"pārveidot par vesela tipa skaitli :-( ")
+        print("Lūdzu ievadiet skaitli vēlreiz")
+
+if (a == 5):
+    print(a**100)
+    print("Aprēķins ir gatavs")
+print("Šis teksts atrodas ārpus darbību bloka - pierakstīts bez",\
+      "atstarpēm priekšā, tāpēc tas parādīsies jebkurā gadījumā")
+
+#paziime = True
+#while paziime:
+#    try:
+#        a = int(input())
+#        paziime = False
+#    except:
+#        print("Diemžēl, cienījamais lietotāj, to, kas ievdīts, nevar",\
+#"pārveidot par vesela tipa skaitli :-( ")
+#        print("Lūdzu ievadiet skaitli vēlreiz")
 
